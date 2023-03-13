e97703f4e91b0d1637beeb5ca6cbc9305c56795d
unknown <santifp1997@gmail.com>
2023-03-13 19:57:25 -0300

    update

--- src/logic/useShowOperation.jsx
+++ src/logic/useShowOperation.jsx
@@ -1,24 +1,36 @@
 import { useState } from "react";
 
 const useShowOperation = () => {
+  const [operation, setOperation] = useState("");
+  const [textState, setTextState] = useState("");
 
-    const [operation,setOperation] = useState();
-    let signs = ["+", "-", "x", "÷", "="];
+  let signsArray = ["+", "-", "x", "÷"];
 
-    const showOperation = (text) => {
+  const showOperation = (text) => {
+    setTextState(text);
 
-        if (text.match(/[0-9]+/)) {
-            console.log(text);
-        }
-        setOperation(text)
-
-
-
-    };
-    return {
-        operation,
-        showOperation
+    if (text.match(/[0-9]+/)) {
+      setOperation((prevState) =>
+        !prevState || signsArray.includes(text) ? `${text}` : `${prevState}${text}`
+      );
     }
+
+    if (signsArray.includes(text) && operation) {
+      if (signsArray.includes(textState)) {
+        setOperation((prevState) => {
+          let arr = prevState.split("");
+          arr.splice(prevState.length - 2, 1, text);
+          return arr.join("");
+        });
+      } else if(text !== '=' && textState !== '='){
+        setOperation((prevState) => `${prevState} ${text} `);
+      }
+    }
+  };
+  return {
+    operation,
+    showOperation,
+  };
 };
 
 export default useShowOperation;
