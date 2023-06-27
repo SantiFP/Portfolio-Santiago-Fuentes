7e879351b2203c3514b59e106d613c7e90570793
unknown <santifp1997@gmail.com>
2023-06-27 01:44:33 -0300

    weatherApp

--- src/components/WeatherDetails.tsx
+++ src/components/WeatherDetails.tsx
@@ -36,14 +36,19 @@ const WeatherDetails: React.FC<Props> = ({
 
   return (
     <div
-      onDoubleClick={removeCity}
-      className={` flex flex-col items-center mt-6 w-3/4 mx-auto border-2 mb-2 border-solid border-blue-300 lg:w-1/5`}
+      className={` flex flex-col items-center mt-6 w-3/4 mx-auto border-2 mb-2 pb-4 border-solid border-blue-300 lg:w-1/5`}
     >
       <div className="flex flex-row w-full">
-        <div className=" text-xl w-5/6 text-center pb-6">
-          <p className="w-5/6 ml-auto mt-2">
-            {name}
-          </p>
+        <div className="w-1/6 pt-1 pl-1">
+          <img
+            onClick={removeCity}
+            className="h-6 w-6"
+            src="/boton-eliminar.png"
+            alt="delete"
+          />
+        </div>
+        <div className=" text-xl w-4/6 pb-6 text-center">
+          <p className="mt-2 ">{name}</p>
         </div>
         <div className="w-1/6 flex flex-col">
           <img
@@ -69,10 +74,18 @@ const WeatherDetails: React.FC<Props> = ({
       <FavAlert isFav={fav}>
         <p className="fav -mt-10">FAVORITES</p>
       </FavAlert>
-      <p className="-mt-1">Temperature: {temp}°</p>
-      <p>Feels like: {feelsLike}°</p>
-      <p>Humidity: {humidity}%</p>
-      <p>Weather: {weather}</p>
+
+      <div className="bg-blue-600 w-full mt-2 py-2 text-white">
+      <div className="flex flex-row  space-x-5 w-full pl-3 mb-2">
+        <p className="w-1/2">Degrees: {temp}°</p>
+        <p className="w-1/2">Feels like: {feelsLike}°</p>
+      </div>
+      <div className="flex flex-row  space-x-5 w-full pl-3">
+        <p className="w-1/2 ">Humidity: {humidity}%</p>
+        <p className="w-1/2">Weather: {weather}</p>
+      </div>
+      </div>
+     
     </div>
   );
 };
